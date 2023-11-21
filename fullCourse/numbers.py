549fa4a75a3edf758c8eacf872971102c9a56ebc
robotictitans <120498502+cansancaojennifer@users.noreply.github.com>
2023-11-21 14:51:05 -0300

    numbers

---
+++ fullCourse/numbers.py
@@ -0,0 +1,26 @@
+from math import *
+
+print(2)
+print(2.0987)
+print(-2.0987)
+print(3 + 4.5)
+print(3 - 4.5)
+print(3 / 4.5)
+print(3 * 4.5)
+print(3 * 4 + 5)
+print(3 * (4 + 5))
+print(10 % 3)
+my_num = 17
+print(my_num)
+print(str(my_num) + " my favorite number")
+absolute_num = -5
+print(abs(absolute_num))
+print(pow(3, 2))
+print(max(4, 6))
+print(min(4, 6))
+print(round(7.8))
+
+#math import
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
