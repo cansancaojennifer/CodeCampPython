dc29342b273b986b44c9b727eacc0f905248cfdb
robotictitans <120498502+cansancaojennifer@users.noreply.github.com>
2023-11-21 12:40:02 -0300

    strings

---
+++ fullCourse/strings.py
@@ -0,0 +1,10 @@
+phrase = "Python Course"
+print(phrase + " is cool")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[7])
+print(phrase.index("h"))
+print(phrase.replace("Course", "is cool"))
