fc009d115f954375d5654037ecbed502682e5920
robotictitans <120498502+cansancaojennifer@users.noreply.github.com>
2023-12-02 17:51:35 -0300

    if statements

--- fullCourse/if.py
+++ fullCourse/if.py
@@ -5,3 +5,8 @@ if is_male or is_tall:
     print("You're a male or tall or both.")
 else:
     print("You're neither male or tall.")
+
+if is_male and is_tall:
+    print("You're a tall male.")
+else:
+    print("You're either not male or not tall or both.")
