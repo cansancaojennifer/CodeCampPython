0b67d361387c38e985f37a16cd9d58e76e25269a
robotictitans <120498502+cansancaojennifer@users.noreply.github.com>
2023-12-03 19:22:22 -0300

    guessing game

--- fullCourse/guessingGame.py
+++ fullCourse/guessingGame.py
@@ -1,7 +1,17 @@
 secret_word = "giraffe"
 guess = ""
+guess_count = 0
+guess_limit = 5
+out_of_guesses = False
 
-while guess != secret_word:
-    guess = input("enter guess: ")
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("enter guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
 
-print("You win!")
+if out_of_guesses:
+    print("Out of guesses, you lose!")
+else:
+    print("You win!")
