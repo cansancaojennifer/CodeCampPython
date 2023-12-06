7661600b6fa0fa15e0b5a81b1bff595ac1623ebd
robotictitans <120498502+cansancaojennifer@users.noreply.github.com>
2023-12-06 11:05:18 -0300

    object functions

--- classes/main.py
+++ classes/main.py
@@ -1,7 +1,9 @@
 from Student import Student
 
-student1 = Student("Joe", "Law", "2.3", True)
-student2 = Student("Jessy", "Engineer", "3.4", False)
+student1 = Student("Joe", "Law", 2.3, True)
+student2 = Student("Jessy", "Engineer", 3.4, False)
+student3 = Student("John", "Accounting", 4.7, False)
 
 print(student1.name)
 print(student2.gpa)
+print(student3.on_honor_roll())
